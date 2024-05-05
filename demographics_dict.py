2181fec174237466badce54dab6af230ebf52264
msamylea <msamymarks@gmail.com>
2024-05-05 08:14:01 -0400

    Add files via upload

---
+++ demographics_dict.py
@@ -0,0 +1,20 @@
+age_groups = ['<18', '18-24', '25-34', '35-44', '45-54', '55-64', '65+']
+
+gender_groups = ['male', 'female', 'non-binary']
+
+geographic_location = ['North Eastern US', 'Central US', 'South East US', 'West US']
+
+education_levels = ['High School', 'Some College', 'Associates Degree', 'Bachelors Degree', 'Masters Degree', 'Doctoral Degree']
+
+employment_status = ['Employed', 'Unemployed', 'Student', 'Retired', 'Other']
+
+income_levels = ['<20k', '20k-40k', '40k-60k', '60k-80k', '80k-100k', '100k-120k', '120k-140k', '140k-160k', '160k-180k', '180k-200k', '200k+']
+
+marital_status = ['Married', 'Single', 'Divorced', 'Widowed']
+
+number_of_children = ['0', '1', '2', '3', '4+']
+
+occupation = ['Management', 'Sales', 'Clerical', 'Service', 'Production', 'Technical', 'Other']
+
+hobbies = ['Sports', 'Music', 'Reading', 'Cooking', 'Traveling', 'Exercise', 'Pets', 'Shopping', 'Movies', 'Gardening', 'Games', 'Other']
+
