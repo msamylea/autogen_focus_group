cc3f3b181e38b3ef75ecbdde7b8e47fa4393a2a2
Eric Z <ezsomething@gmail.com>
2024-07-14 08:54:09 -0500

    changes to unify model selection, some i/o
    - update config to have name of ollamma model and use it everywhere
    - fix persona json script for syntax
    - strip colors from focus group and final UX dialogs (hard to see on some browsers)
    - update final results to write to a markdown file in docs (placeholder there now)

--- pages/Analyze_Final_Results.py
+++ pages/Analyze_Final_Results.py
@@ -16,8 +16,6 @@ with stylable_container(
         key="green_button",
         css_styles="""
             button {
-                background-color: teal;
-                color: white;
                 box-shadow: 2px 0 7px 0 grey;
             }
             """,
@@ -36,14 +34,13 @@ with stylable_container(
                     css_styles="""
                         {
                             border: 2px solid rgba(49, 51, 63, 0.2);
-                            background: offwhite;
                             border-radius: 0.5rem;
                             padding: calc(1em - 1px);
                             box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
                         }
                         """,
                 ):
-                    st.markdown("<h1 style='text-align: center; color: black;'>Analysis of Group Chat</h1>", unsafe_allow_html=True)
+                    st.markdown("<h1 style='text-align: center; ;'>Analysis of Group Chat</h1>", unsafe_allow_html=True)
                     st.markdown("<h4 style='text-align: center; color: grey;'>The following is a summary of the focus group chat.</h4>", unsafe_allow_html=True)
 
                 llm = cfg.completions_model
@@ -53,7 +50,7 @@ with stylable_container(
                         {"role": "system",
                         "content": f"Analyze the focus group chat and provide a detailed summary and analysis of the discussion in markdown format. Chat: {summary}"},
                     ],
-                    model="l3custom"
+                    model=cfg.llama_model
                 )
 
                 analysis = response.choices[0].message.content
@@ -63,7 +60,6 @@ with stylable_container(
                     css_styles="""
                         {
                             border: 2px solid rgba(49, 51, 63, 0.2);
-                            background: offwhite;
                             border-radius: 0.5rem;
                             padding: calc(1em - 1px);
                             box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
@@ -74,3 +70,5 @@ with stylable_container(
 
                         st.markdown(analysis, unsafe_allow_html=True)
 
+                with open("./docs/final_analysis.md", 'w') as f:
+                    f.write(analysis)
